b8ad211fa9411b0f16f92b981b7de2ea330a6825
Relari <renzolavador@gmail.com>
2025-01-16 13:29:30 -0500

    feat(proyect): Creacion del Proyecto

---
+++ src/main/java/pe/com/scotiabank/code_challenge_scotiabank_v1/CodeChallengeScotiabankV1Application.java
@@ -0,0 +1,13 @@
+package pe.com.scotiabank.code_challenge_scotiabank_v1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodeChallengeScotiabankV1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodeChallengeScotiabankV1Application.class, args);
+	}
+
+}
---
+++ src/test/java/pe/com/scotiabank/code_challenge_scotiabank_v1/CodeChallengeScotiabankV1ApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.com.scotiabank.code_challenge_scotiabank_v1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodeChallengeScotiabankV1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
